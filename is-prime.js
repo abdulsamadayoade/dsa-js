0c8af883a1301b23eb57c95ddc80629f875dcdf3
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2026-01-04 23:00:51 +0100

    🔥

---
+++ is-prime.js
@@ -0,0 +1,38 @@
+// Write a function, isPrime, that takes in a number as an argument. The function should return a boolean indicating whether or not the given number is prime.
+// A prime number is a number that is only divisible by two distinct numbers: 1 and itself.
+// For example, 7 is a prime because it is only divisible by 1 and 7. For example, 6 is not a prime because it is divisible by 1, 2, 3, and 6.
+// You can assume that the input number is a positive integer.
+
+/**
+ * @param {number} n
+ * @returns {boolean}
+ */
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+
+  if (n % 2 === 0 || n % 3 === 0) return false;
+
+  const sqrt = Math.floor(Math.sqrt(n));
+
+  for (let i = 5; i <= sqrt; i += 2) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+// Test Scenarios
+// isPrime(2); // -> true
+// isPrime(3); // -> true
+// isPrime(4); // -> false
+// isPrime(5); // -> true
+// isPrime(6); // -> false
+// isPrime(7); // -> true
+// isPrime(8); // -> false
+// isPrime(25); // -> false
+// isPrime(31); // -> true
+// isPrime(2017); // -> true
+// isPrime(2048); // -> false
+// isPrime(1); // -> false
+// isPrime(713); // -> false
